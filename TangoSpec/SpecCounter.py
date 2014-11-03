4d8ff98019419e84c6e6b6e17ad604146df03d05
coutinho <coutinho@esrf.fr>
2014-11-03 17:08:42 +0100

    Implement State, Status and Value events

--- TangoSpec/SpecCounter.py
+++ TangoSpec/SpecCounter.py
@@ -11,9 +11,12 @@
 
 """A TANGO device server for SPEC based on SpecClient."""
 
+import time
 import logging
 
-from PyTango import Util, Attr, DevState, CmdArgType, AttrWriteType, DebugIt
+from PyTango import Util, DevState, CmdArgType
+from PyTango import AttrWriteType, AttrQuality
+from PyTango import DebugIt
 from PyTango.server import Device, DeviceMeta, attribute, command
 from PyTango.server import device_property
 
@@ -21,7 +24,7 @@ from SpecClient_gevent.SpecCounter import SpecCounter as _SpecCounter
 from SpecClient_gevent.SpecClientError import SpecClientError
 
 from . import TgGevent
-from .SpecCommon import SpecState_2_TangoState, execute, switch_state
+from .SpecCommon import SpecCounterState_2_TangoState, execute, switch_state
 
 
 class SpecCounter(Device):
@@ -55,7 +58,7 @@ class SpecCounter(Device):
         Device.init_device(self)
         self.set_change_event("State", True, True)
         self.set_change_event("Status", True, False)
-        #self.set_change_event("Value", True, False)
+        self.set_change_event("Value", True, False)
 
         switch_state(self, DevState.INIT,
                      "Pending connection to " + self.SpecCounter)
@@ -86,16 +89,47 @@ class SpecCounter(Device):
 
         self.__spec_version_name = spec_version
         self.__spec_counter_name = counter
-        
+
+        cb = dict(connected=self.__counterConnected,
+                  disconnected=self.__counterDisconnected,
+                  counterValueChanged=self.__counterValueChanged,
+                  counterStateChanged=self.__counterStateChanged)
         try:
             self.__spec_counter = TgGevent.get_proxy(_SpecCounter,
                                                      counter,
-                                                     spec_version)
+                                                     spec_version,
+                                                     callbacks=cb)
+            channel_value_name = "scaler/{0}/value".format(counter)
             msg = "Connected to counter " + self.SpecCounter
             switch_state(self, DevState.ON, msg)
         except SpecClientError as spec_error:
             status = "Error connecting to Spec counter: %s" % str(spec_error)
             switch_state(self, DevState.FAULT, status)
+
+    def __counterConnected(self):
+        switch_state(self, DevState.ON, "Connected to counter " + self.SpecCounter)
+
+    def __counterDisconnected(self):
+        switch_state(self, DevState.OFF, "counter disconnected")
+
+    def __counterStateChanged(self, spec_state):
+        old_state = self.get_state()
+        state = SpecCounterState_2_TangoState[spec_state]
+
+        # Fire a value event with VALID quality
+        if old_state == DevState.RUNNING and state != DevState.RUNNING:
+            value = self.__spec_counter.getValue()
+            execute(self.push_change_event, "Value", value)
+
+        # switch tango state and status attributes and send events
+        switch_state(self, state, "Counter is now %s" % (str(state),))
+
+    def __counterValueChanged(self, value):
+        if self.get_state() == DevState.RUNNING:
+            execute(self.push_change_event, "Value", value,
+                    time.time(), AttrQuality.ATTR_CHANGING)
+        else:
+            execute(self.push_change_event, "Value", value)
 
     def read_Value(self):
         return self.spec_counter.getValue()
