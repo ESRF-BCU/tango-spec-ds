17c9be2922f150e3436267ca04d97a989543db46
coutinho <coutinho@esrf.fr>
2015-06-18 08:43:44 +0200

    Change version to 2.0; Make Tiago as contact person

--- TangoSpec/__init__.py
+++ TangoSpec/__init__.py
@@ -15,9 +15,9 @@ __this_year = datetime.date.today().year
 del datetime
 
 __project__ = 'TangoSpec'
-__version_info__ = (1, 2, 0, 'final', 0)
+__version_info__ = (2, 0, 0, 'final', 0)
 __version__ = "{0[0]}.{0[1]}.{0[2]}".format(__version_info__)
-__authors__ = ['Andy Gotz', 'Tiago Coutinho', 'Matias Guijarro']
+__authors__ = ['Tiago Coutinho', 'Matias Guijarro', 'Andy Gotz']
 __author__ = __authors__[0]
 __copyright__ = '{0}, European Synchrotron Radiation Facility'.format(__this_year)
 __description__ = __doc__
